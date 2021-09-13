0923f79cdcf9c73f8040068e4af2dbe2d162ce26
Savannbhuva <savanbhuva098@gmail.com>
2021-09-13 18:25:54 +0700

    Update Arrays.js

--- Basics/Arrays.js
+++ Basics/Arrays.js
@@ -4,4 +4,4 @@ selectedColors[2] = 'green';
 selectedColors[3] = 1;
 console.log(selectedColors);
 
-//Array is a list of Structur
+//Array is a list of Structure that we use to represent a List of Items
